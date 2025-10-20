edab17ed72f3bff105c11b14df85920cf690b1e2
Abdur Rehman <syed.ebad1999@gmail.com>
2025-10-21 03:27:32 +0500

    chore: added layout and navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,55 @@
-import { Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from '../src/components/Loading';
+import PokemonDetails from '../src/screens/PokemonDetails';
+import PokemonList from '../src/screens/PokemonList';
+import { persistor, store } from '../src/store/store';
+import { RootStackParamList } from '../src/types/pokemon';
 
-export default function Index() {
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="PokemonList"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#FFFFFF',
+              },
+              headerTintColor: '#1E293B',
+              headerTitleStyle: {
+                fontWeight: '600',
+              },
+              headerShadowVisible: false,
+            }}
+          >
+            <Stack.Screen
+              name="PokemonList"
+              component={PokemonList}
+              options={{
+                title: 'Pokemon',
+                headerLargeTitle: true,
+              }}
+            />
+            <Stack.Screen
+              name="PokemonDetails"
+              component={PokemonDetails}
+              options={{
+                headerBackTitle: '',
+              }}
+            />
+          </Stack.Navigator>
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
+    </SafeAreaProvider>
   );
 }
