56db128f651e407e9d0cc7ced72b86612c8287dc
Abdur Rehman <syed.ebad1999@gmail.com>
2025-10-21 03:44:53 +0500

    test: configure test setup

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,15 +1,8 @@
-import 'react-native-gesture-handler/jestSetup';
-import '@testing-library/react-native/extend-expect';
 
-// Mock react-native modules
-jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
-
-// Mock AsyncStorage
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
 
-// Mock Expo Constants
 jest.mock('expo-constants', () => ({
   expoConfig: {
     extra: {
@@ -18,33 +11,64 @@ jest.mock('expo-constants', () => ({
   }
 }));
 
-// Mock React Navigation
-jest.mock('@react-navigation/native', () => {
-  const actualNav = jest.requireActual('@react-navigation/native');
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({
+    navigate: jest.fn(),
+    goBack: jest.fn(),
+    setOptions: jest.fn(),
+  }),
+  useRoute: () => ({
+    params: {
+      pokemonName: 'pikachu',
+      pokemonId: 25,
+    },
+  }),
+  NavigationContainer: ({ children }) => children,
+}));
+
+jest.mock('@react-navigation/native-stack', () => ({
+  createNativeStackNavigator: () => ({
+    Navigator: ({ children }) => children,
+    Screen: ({ children }) => children,
+  }),
+}));
+
+// Mock reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+// Mock gesture handler
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
   return {
-    ...actualNav,
-    useNavigation: () => ({
-      navigate: jest.fn(),
-      goBack: jest.fn(),
-      setOptions: jest.fn(),
-    }),
-    useRoute: () => ({
-      params: {
-        pokemonName: 'pikachu',
-        pokemonId: 25,
-      },
-    }),
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
   };
 });
-
-// Mock Image component
-jest.mock('react-native', () => {
-  const RN = jest.requireActual('react-native');
-  RN.Image = ({ children, ...props }) => React.createElement('Image', props, children);
-  return RN;
-});
-
-// Silence the warning: Animated: `useNativeDriver` is not supported
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-
-global.__reanimatedWorkletInit = jest.fn();
